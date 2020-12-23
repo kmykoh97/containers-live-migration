09c6630297c576ac73dee69252f74be75b19fea8
kmykoh <kmykoh97@gmail.com>
2020-12-24 01:23:48 +0800

    added test script

---
+++ test/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# sudo nano /etc/docker/daemon.json
+{
+  "experimental":true
+}
+# sudo systemctl restart docker
+sudo add-apt-repository ppa:criu/ppa
+
+sudo apt-get update
+sudo apt-get install -y criu
+sudo criu check
